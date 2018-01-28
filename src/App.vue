4f0f18d33e854b938fb08f6299816e5bad5ea295
Dominik <dom.guzy@gmail.com>
2018-01-28 14:24:20 +0100

    init, counter

---
+++ src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <div id="app">
+    <section class="hero is-light">
+      <div class="hero-body">
+        <div class="container">
+          <img src="./assets/logo.png">
+          <h1 class="title">{{ msg }}</h1>
+        </div>
+      </div>
+    </section>
+    <Counter></Counter>
+  </div>
+</template>
+
+<script>
+ import Counter from './Counter.vue'
+  export default {
+    name: 'app',
+    data() {
+      return {
+        msg: `Welcome to Gorrion's Sample Vue.js App`,
+      };
+    },
+    components: {
+      Counter
+    }
+  };
+</script>
+
+<style>
+  
+</style>
