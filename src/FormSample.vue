c5644cf166c638a7adf1e8ad0d269d44c3a82643
Dominik <dom.guzy@gmail.com>
2018-01-28 20:14:16 +0100

    FormSample component

---
+++ src/FormSample.vue
@@ -0,0 +1,33 @@
+<template>
+  <section class="section">
+    <div class="container">
+      <div class="columns">
+        <div class="column">
+          <div class="field">
+            <label class="label">Name</label>
+            <div class="control">
+              <input class="input" type="text" v-model="name">
+            </div>
+          </div>
+          <div class="field">
+            <label class="label">Emplotyer</label>
+            <div class="control">
+              <input class="input" type="text" v-model="employer">
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+          name: "Dominik",
+          employer: "Gorrion"
+        };
+    }
+};
+</script>
